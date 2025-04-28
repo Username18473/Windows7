4f323e4c0103e0352aad87faf230cdf0f5c03a18
Isandro <Isandro-T@outlook.com>
2025-04-27 21:50:24 -0400

    Add files via upload

---
+++ redirection.js
@@ -0,0 +1,20 @@
+function openNotepadApp() {
+            window.location.href = "Notepad.html";
+        }
+
+function openPaintApp() {
+            window.location.href = "Paint.html";
+        }
+        
+function openWebcamApp() {
+            window.location.href = "webcam.html";
+        } 
+
+function openMazeApp() {
+            window.location.href = "Maze.html";
+        } 
+
+function openMediaPlayerApp() {
+            window.location.href = "Mediaplayer.html";
+        }
+        
---
+++ wallpaper.js
@@ -0,0 +1,37 @@
+// Sets the desktop wallpaper and saves the preference in local storage
+function setWallpaper(wallpaper) {
+    document.documentElement.style.setProperty('--background-image', `url('${wallpaper}')`);
+    localStorage.setItem('selectedWallpaper', wallpaper);
+}
+
+// Loads the wallpaper from local storage when the page is loaded
+function loadWallpaper() {
+    const savedWallpaper = localStorage.getItem('selectedWallpaper');
+    if (savedWallpaper) {
+        document.documentElement.style.setProperty('--background-image', `url('${savedWallpaper}')`);
+    } else {
+        document.documentElement.style.setProperty('--background-image', `url('path/to/Harmony.png')`);
+    }
+}
+
+// Triggers the file upload process for setting a custom wallpaper
+function triggerUpload() {
+    document.getElementById('upload-image').click(); 
+}
+
+// Sets a custom wallpaper from an uploaded file
+function setCustomWallpaper(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            document.body.style.backgroundImage = `url('${e.target.result}')`;
+            document.body.style.backgroundSize = 'cover'; 
+        };
+        reader.readAsDataURL(file);
+        localStorage.setItem('selectedWallpaper', e.target.result);
+    }
+}
+
+// Load wallpaper on DOMContentLoaded
+window.addEventListener('DOMContentLoaded', loadWallpaper);
