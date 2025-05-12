c76d435f71870c382acd2d3eff034d905a86bb50
Isandro <Isandro-T@outlook.com>
2025-05-12 08:20:59 -0400

    Update redirection.js

--- scripts/redirection.js
+++ scripts/redirection.js
@@ -1,22 +1,3 @@
-function openNotepadApp() {
-            window.location.href = "Notepad.html";
-        }
-
-function openPaintApp() {
-            window.location.href = "Paint.html";
-        }
-        
-function openWebcamApp() {
-            window.location.href = "webcam.html";
-        } 
-
-function openMazeApp() {
-            window.location.href = "Maze.html";
-        } 
-
-function openMediaPlayerApp() {
-            window.location.href = "Mediaplayer.html";
-        }
 
 function logoff() {
     window.location.href = "Logoff.html";
