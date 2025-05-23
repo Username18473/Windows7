5ae8ff96dc691ac1f7b6e3aaeccff0313b1531a5
Isandro <Isandro-T@outlook.com>
2025-05-22 23:22:29 -0400

    Update redirection.js

--- scripts/redirection.js
+++ scripts/redirection.js
@@ -1,13 +1,8 @@
 document.getElementById("power-option").addEventListener("change", function() {
     let action = this.value;
-    
     if (action === "logoff" || action === "switch-user") {
-        logoff();
+        window.location.href = "Logoff.html";
     } else if (action === "shutdown") {
         window.location.href = "about:blank";
     }
 });
-
-function logoff() {
-    window.location.href = "Logoff.html"; // Redirects to Logoff page
-}
